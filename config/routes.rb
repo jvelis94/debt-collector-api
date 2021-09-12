e76c273701594a44cb0bbf965a8595c4ac8c775a
Joel Velis <joelvelis94@gmail.com>
2021-09-11 23:51:08 -0400

    chnaged routes for login and register

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,27 @@ Rails.application.routes.draw do
 
   end
 
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: %w[show]
+  end
+
   devise_for :users,
-            controllers: {
-                sessions: 'users/sessions',
-                registrations: 'users/registrations'
-            }
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'api/login',
+      sign_out: 'api/logout',
+      registration: 'api/signup'
+    },
+    controllers: {
+      sessions: 'sessions',
+      registrations: 'registrations'
+    }
+
+  # devise_for :users,
+  #           controllers: {
+  #               sessions: 'users/sessions',
+  #               registrations: 'users/registrations'
+  #           }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
