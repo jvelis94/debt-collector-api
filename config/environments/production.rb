342b762641261f46b02f116c619f3cdb22cf2b8b
Joel Velis <joelvelis94@gmail.com>
2021-09-12 19:38:53 -0400

    changed domain setting in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,10 +121,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "debt-collector-api-wts7v.ondigitalocean.app",
     user_name: ENV["GMAIL"],
     password: ENV["GMAIL_PASSWORD"],
-    authentication: :login,
+    authentication: :plain,
     enable_starttls_auto: true
   }
+  
 end
