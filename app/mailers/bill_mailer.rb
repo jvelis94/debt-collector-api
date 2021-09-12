9ba7d9e14b67a110b4d5b62e495881a47a89a7fd
Joel Velis <joelvelis94@gmail.com>
2021-09-12 18:56:18 -0400

    added default from for mailer

--- app/mailers/bill_mailer.rb
+++ app/mailers/bill_mailer.rb
@@ -2,6 +2,6 @@ class BillMailer < ActionMailer::Base
     def new_bill
         puts "NEW BILL"
         @bill = params[:bill]
-        mail(to: @bill.user.email, subject: "Have you been paid for #{@bill.bill_name} charges?")
+        mail(from: "velisjoel@gmail.com", to: @bill.user.email, subject: "Have you been paid for #{@bill.bill_name} charges?")
     end
 end
