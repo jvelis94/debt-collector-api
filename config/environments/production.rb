eb33e7ad4da1de752e6ae311c4dc2b826cb5964a
Joel Velis <joelvelis94@gmail.com>
2021-09-12 20:40:04 -0400

    changed setting in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,11 +121,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "debt-collector-api-wts7v.ondigitalocean.app",
+    domain: "gmail.com",
     user_name: ENV["GMAIL"],
     password: ENV["GMAIL_PASSWORD"],
-    authentication: :plain,
+    authentication: "plain",
     enable_starttls_auto: true
   }
-  
 end
