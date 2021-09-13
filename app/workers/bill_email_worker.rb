1c610dcac2bd36417ed132b69b14f77b5777904f
Joel Velis <joelvelis94@gmail.com>
2021-09-13 16:34:24 -0400

    changed requirements for receiving an email

--- app/workers/bill_email_worker.rb
+++ app/workers/bill_email_worker.rb
@@ -2,7 +2,10 @@ class BillEmailWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    open_bills = Bill.joins(:bill_recipients).where("bill_recipients.is_paid is ?", false).uniq
+    # open_bills = Bill.joins(:bill_recipients).where("bill_recipients.is_paid is ?", false).uniq
+    open_bills = Bill.joins(:bill_recipients)
+      .where(bill_recipients: { is_paid: false})
+      .where.not(bill_recipients: { total_owes: 0 }).uniq
     open_bills.each do |bill|
       BillMailer.with(bill: bill).new_bill.deliver_later
     end
--- lib/tasks/bill.rake
+++ lib/tasks/bill.rake
@@ -1,6 +1,6 @@
 namespace :bill_email do 
     desc "Sends scheduled emails"
-    task :send_scheduled_bill_email => :enviroment do 
-      BillMailer.new_bill
+    task :send_scheduled_bill_email => :environment do 
+      BillEmailWorker.new.perform
     end
   end
